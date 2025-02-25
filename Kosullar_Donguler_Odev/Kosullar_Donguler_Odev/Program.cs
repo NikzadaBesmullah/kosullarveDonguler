e627d584623844f9cfe460dacf0ada8d9f1200b7
Besmullah Nikzada <besmullahnikzada786@gmail.com>
2025-02-25 17:15:05 +0300

    İlk yukleme

---
+++ Kosullar_Donguler_Odev/Kosullar_Donguler_Odev/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosullar_Donguler_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir Sayının tek-çift ve pzoitif-negatif kontrolu
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0) { Console.WriteLine("Girdiğiniz Sayı Pozitif."); }
+             else if (sayi < 0) { Console.WriteLine("Girdiğiniz Sayı Negatif."); }
+            else { Console.WriteLine("Girdiğiniz Sayı Sıfır."); }
+            //Math.Abs: Mutlak degerini alır
+            switch (Math.Abs(sayi % 2))
+            {
+                case 0:
+                    Console.WriteLine("Çift Sayı Girdiniz.");
+                    break;
+                case 1:
+                    Console.WriteLine("Tek Sayı Girdiniz.");
+                    break;
+
+            }
+
+            Console.WriteLine("--------------------------------");
+            // 1-100 arasındaki çift sayıları ekrana yazdırma
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 50)
+                    break;
+
+                if (i % 2 != 0)
+                    continue;
+
+                if (i % 10 == 0)
+                    continue;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("--------------------------------");
+
+
+            //foreach kullanımı
+            string[] isimler = { "Mehmet", "Ali", "yusuf", "Fatma", "Muhammed", "Fırat" };
+            foreach (string s in isimler)
+            {
+                if (s == "yusuf") continue;
+                Console.WriteLine(s);
+
+            }
+
+        }
+    }
+}
---
+++ Kosullar_Donguler_Odev/Kosullar_Donguler_Odev/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kosullar_Donguler_Odev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kosullar_Donguler_Odev")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e3878e6-c836-4dd4-af08-dc2b2d62c2af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
